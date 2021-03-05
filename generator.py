c4ded238328a278ff4dc91f4e3e75184b7ad9e3f
loliconshik3 <loliconshik3@gmail.com>
2021-03-06 02:35:45 +0300

    mini fixes

--- generator.py
+++ generator.py
@@ -3,16 +3,17 @@ import random
 def RandomNick(length: int):
     alpha_first = 'AEIOUYaeiouy'
     alpha_second = 'BCDFGHJKLMNPQRSTVWXZbcdfghjklmnpqrstvwxz'
-    s = ''
-    i = 1
+    s = ''; i = 1
 
     r = random.randint(0.00, 100.00)
     first_char = ''
+
     if (r > 50.00):
         first_char = random.choice(alpha_first)
     else:
         first_char = random.choice(alpha_second)
     s += first_char
+
     while (length - 1) >= i:
         char = s[len(s)-1]
         if (char in alpha_first):
@@ -28,30 +29,28 @@ def NickGenerator():
     if (choice == "4"):
         exit()
     limit = int(input("\nСколько никнеймов вы хотите получить?\n"))
-    file = ''
-    s = '\n'
-    i = 1
+    file = ''; i = 1
 
+    print('\n')
     if (choice == "1"):
         file = 'boy_users.txt'
     elif (choice == "2"):
         file = 'girl_users.txt'
+
     elif (choice == "3"):
         length = int(input('\nВведите длину ника.\n'))
         while limit >= i:
-            s += f"{i}. {RandomNick(length)}\n"
+            nick = RandomNick(length)
+            print(f"{i}. {nick}")
             i += 1
-
 
     if (file != ''):
         fl = open(file, 'r', encoding='UTF-8')
         lines = fl.readlines()
         while limit >= i:
             nick = random.choice(lines)
-            s += f"{i}. {nick}"
+            print(f"{i}. {nick}")
             i += 1
-
-    print(s)
 
     NickGenerator()
 
